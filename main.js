27bd609f3bdcbbd4457bc7c89ac35332782f4ea9
JasonCB083 <jason.cllr@gmail.com>
2021-02-14 23:38:20 +0100

    dark n light mode with star wars toggle

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const modeBtn = document.querySelector ("#mode-btn-");
+/*const modeBtn2 = document.querySelector ("#mode-btn");*/
 
 /*
 modeBtn.addEventListener("click", e => {
@@ -8,7 +8,14 @@ modeBtn.addEventListener("click", e => {
 });
 */
 
+
+const modeBtn = document.querySelector ("#mode-btn-");
+
 document.querySelector('#mode-btn-').onchange = (e) => {
   const checked = e.target.checked;
   document.body.classList.toggle("light")
+
+  /*modeBtn2.innerText = document.body.classList.contains ("light") ? "light Mode" : "Dark Mode ";*/
+
+
 };
